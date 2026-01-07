c30c8bac32736777c45276fbf302ab953bd599ba
Alan Nevot <alanccbp@gmail.com>
2026-01-07 18:59:58 -0300

    Update companies.routes.ts

--- services/users/src/modules/companies/companies.routes.ts
+++ services/users/src/modules/companies/companies.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { CompaniesController } from './companies.controller';
 import { authMiddleware } from '../../middlewares/auth.middleware';
-import { companyContext } from '@/src/middlewares/companyContext.middleware';
+import { requireCompanyContext } from '@/src/middlewares/requireCompanyContext.middleware';
 import { requireCompanyRole } from '@/src/middlewares/requireCompanyRole.middleware';
 import { CompanyRole } from '@/prisma/generated/prisma/enums';
 import { MembershipsController } from '../memberships/memberships.controller';
@@ -9,20 +9,23 @@ import { MembershipsController } from '../memberships/memberships.controller';
 const router = Router();
 
 router.post('/', authMiddleware, CompaniesController.create);
+
 router.get('/my', authMiddleware, CompaniesController.myCompanies);
+
 router.post(
   '/invite',
   authMiddleware,
-  companyContext,
+  requireCompanyContext,
   requireCompanyRole(CompanyRole.OWNER, CompanyRole.ADMIN),
   MembershipsController.invite,
 );
 
 router.post('/accept', authMiddleware, MembershipsController.accept);
+
 router.patch(
   '/:id',
   authMiddleware,
-  companyContext,
+  requireCompanyContext,
   requireCompanyRole(CompanyRole.OWNER, CompanyRole.ADMIN),
   CompaniesController.update,
 );
